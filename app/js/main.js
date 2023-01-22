adbe39801d97d365018d6e3e574f6ba878bd8542
NikolayGrishan <kolyagrishan19@gmail.com>
2023-01-23 02:03:15 +0300

    готовое модальное окно

--- app/js/main.js
+++ app/js/main.js
@@ -1,123 +1,129 @@
 $(document).ready(function () {
-  $(".slider").slick({
-    arrows: true,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    speed: 1000,
-    easing: "lenear",
-    infinite: true,
-    initialSlide: 1,
-    autoplay: true,
-    autoplaySpeed: 3000,
-    pauseOnFocus: true,
-    pauseOnHover: true,
-    draggable: false,
-    swipe: true,
-    touchThreshold: 15,
-    waitForAnimate: false,
-    variableWidth: true,
-    appendArrows: $(".match__arrows"),
+    $(".popup").magnificPopup();
 
-    responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 2,
-          arrows: false,
-          dots: true,
-        },
-      },
-      {
-        breakpoint: 500,
-        settings: {
-          slidesToShow: 1,
-          variableWidth: false,
-          arrows: false,
-          dots: true,
-        },
-      },
-    ],
-  });
+    $("#cross").click(function () {
+        $.magnificPopup.close();
+    });
+
+    $(".slider").slick({
+        arrows: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        speed: 1000,
+        easing: "lenear",
+        infinite: true,
+        initialSlide: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        pauseOnFocus: true,
+        pauseOnHover: true,
+        draggable: false,
+        swipe: true,
+        touchThreshold: 15,
+        waitForAnimate: false,
+        variableWidth: true,
+        appendArrows: $(".match__arrows"),
+
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    arrows: false,
+                    dots: true,
+                },
+            },
+            {
+                breakpoint: 500,
+                settings: {
+                    slidesToShow: 1,
+                    variableWidth: false,
+                    arrows: false,
+                    dots: true,
+                },
+            },
+        ],
+    });
 });
 
 $(document).ready(function () {
-  $(".slider--n").slick({
-    arrows: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    speed: 1000,
-    easing: "lenear",
-    appendArrows: $(".news__arrows"),
-    draggable: false,
-    infinite: true,
-    initialSlide: 1,
-    touchThreshold: 15,
-    waitForAnimate: false,
-    variableWidth: true,
+    $(".slider--n").slick({
+        arrows: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        speed: 1000,
+        easing: "lenear",
+        appendArrows: $(".news__arrows"),
+        draggable: false,
+        infinite: true,
+        initialSlide: 1,
+        touchThreshold: 15,
+        waitForAnimate: false,
+        variableWidth: true,
 
-    responsive: [
-      {
-        breakpoint: 1067,
-        settings: {
-          slidesToShow: 1,
-          arrows: false,
-          variableWidth: true,
-          dots: true,
-        },
-      },
-      {
-        breakpoint: 500,
-        settings: {
-          slidesToShow: 1,
-          variableWidth: false,
-          arrows: false,
-          dots: true,
-        },
-      },
-    ],
-  });
+        responsive: [
+            {
+                breakpoint: 1067,
+                settings: {
+                    slidesToShow: 1,
+                    arrows: false,
+                    variableWidth: true,
+                    dots: true,
+                },
+            },
+            {
+                breakpoint: 500,
+                settings: {
+                    slidesToShow: 1,
+                    variableWidth: false,
+                    arrows: false,
+                    dots: true,
+                },
+            },
+        ],
+    });
 });
 
 document.addEventListener("DOMContentLoaded", function () {
-  function openMobileSidebar() {
-    const burgerBtn = document.querySelector(".header__burger");
-    const closeMenuBtn = document.querySelector(".sidebar__close");
-    const mobMenu = document.querySelector(".sidebar");
+    function openMobileSidebar() {
+        const burgerBtn = document.querySelector(".header__burger");
+        const closeMenuBtn = document.querySelector(".sidebar__close");
+        const mobMenu = document.querySelector(".sidebar");
 
-    burgerBtn.addEventListener("click", () => {
-      mobMenu.style.transform = "translateX(0%)";
-    });
+        burgerBtn.addEventListener("click", () => {
+            mobMenu.style.transform = "translateX(0%)";
+        });
 
-    closeMenuBtn.addEventListener("click", () => {
-      mobMenu.style.transform = "translateX(200%)";
-    });
+        closeMenuBtn.addEventListener("click", () => {
+            mobMenu.style.transform = "translateX(200%)";
+        });
 
-    mobMenu.addEventListener("click", (event) => {
-      if (event.target) {
-        mobMenu.style.transform = "translateX(200%)";
-      }
-    });
-  }
-  openMobileSidebar();
+        mobMenu.addEventListener("click", (event) => {
+            if (event.target) {
+                mobMenu.style.transform = "translateX(200%)";
+            }
+        });
+    }
+    openMobileSidebar();
 
-  function statsTabs() {
-    let statsNav = $(".stats__title--tab");
-    let statsContent = $(".stats__block--tab");
+    function statsTabs() {
+        let statsNav = $(".stats__title--tab");
+        let statsContent = $(".stats__block--tab");
 
-    statsNav.on("click", function (event) {
-      let activeContent = $(this).attr("data-target");
-      statsNav.removeClass("stats__title--tab_active");
-      statsContent.removeClass("stats__block--tab_active");
-      $(activeContent).addClass("stats__block--tab_active");
-      $(this).addClass("stats__title--tab_active");
-    });
-  }
-  statsTabs();
+        statsNav.on("click", function (event) {
+            let activeContent = $(this).attr("data-target");
+            statsNav.removeClass("stats__title--tab_active");
+            statsContent.removeClass("stats__block--tab_active");
+            $(activeContent).addClass("stats__block--tab_active");
+            $(this).addClass("stats__title--tab_active");
+        });
+    }
+    statsTabs();
 
-  function dropDown() {
-    $(".stats__document-title").click(function (event) {
-      $(this).toggleClass("active").next().slideToggle();
-    });
-  }
-  dropDown();
+    function dropDown() {
+        $(".stats__document-title").click(function (event) {
+            $(this).toggleClass("active").next().slideToggle();
+        });
+    }
+    dropDown();
 });
