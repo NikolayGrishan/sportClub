d76393303a2004d53cfd7c537cc890d9d9e21fe4
NikolayGrishan <kolyagrishan19@gmail.com>
2023-01-24 22:21:51 +0300

    реализовано модальное окно поиска на старнице лендинга

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
-    $(".searchingPopup").magnificPopup();
+    $(".searchingPopup").magnificPopup({
+        alignTop: true
+    });
 });
 
 $(document).ready(function () {
